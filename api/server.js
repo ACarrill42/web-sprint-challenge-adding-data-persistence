f34fe19924151a433f01dd0713bb89ea7e490b34
ACarrill42 <austincarrill@gmail.com>
2021-09-26 12:37:50 -0500

    setup index and server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmnet');
+const resourcesRouter = require('./resource/router');
+const taskRouter = require('./task/router');
+const projectRouter = require('./project/router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api', resourcesRouter);
+server.use('/api', taskRouter);
+server.use('/api', projectRouter);
+
+
+module.exports = server;
