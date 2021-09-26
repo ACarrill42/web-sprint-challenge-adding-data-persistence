1c67a553147379692974da2032aeb65b79cc9b4e
ACarrill42 <austincarrill@gmail.com>
2021-09-26 13:23:25 -0500

    fixed migrate file

--- data/migrations/20210926172916_migrations-file.js
+++ data/migrations/20210926172916_migrations-file.js
@@ -3,8 +3,9 @@ exports.up = function(knex) {
   return knex.schema
   .createTable('projects', tbl => {
     tbl.increments('project_id')
-    tbl.primary('project_id')
-    tbl.text('project_name')
+    tbl.integer('project_id')
+      .primary('project_id')
+    tbl.string('project_name')
       .unique()
       .notNullable();        
     tbl.string('project_description')
@@ -14,8 +15,9 @@ exports.up = function(knex) {
 
   .createTable('resources', tbl => {
     tbl.increments('resource_id')
-    tbl.primary('resource_id')
-    tbl.text('resource_name')
+    tbl.integer('resource_id')
+      .primary('resource_id')
+    tbl.string('resource_name')
       .unique()
       .notNullable();
     tbl.string('resource_description')  
@@ -23,20 +25,22 @@ exports.up = function(knex) {
 
   .createTable('tasks', tbl => {
     tbl.increments('task_id')
-    tbl.primary('task_id')
+    tbl.integer('task_id')
+      .primary('task_id')
     tbl.string('task_description')
       .notNullable();
     tbl.text('task_notes')
     tbl.boolean('task_completed')
       .defaultTo(0);
-    tbl.foreign('project_id')
-      .references('projects.project_id');
+    tbl.integer('project_id')
+      .references('projects_id')
+      .inTable('projects')
   });
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('tasks')  
     .dropTableIfExists('projects')
-    .dropTableIfExists('resources')
-    .dropTableIfExists('tasks');
+    .dropTableIfExists('resources');
 };
